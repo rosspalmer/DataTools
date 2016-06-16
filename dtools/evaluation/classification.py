14a67db5c178c29304bc34cd83fe35947590c477
Ross Palmer <palmer@opttek.com>
2016-06-16 12:04:21 -0600

    added binary classification evaluation functions

--- dtools/evaluation/classification.py
+++ dtools/evaluation/classification.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot
 from sklearn.metrics import log_loss, roc_auc_score, accuracy_score
 from sklearn.metrics import precision_score, recall_score, confusion_matrix
+from sklearn.preprocessing import normalize
 
 def summary(actual, predict_prob, thres=0.5, prob_hist=False):
 
@@ -21,18 +22,37 @@ def summary(actual, predict_prob, thres=0.5, prob_hist=False):
 
     print('-----Confusion Matrix-----')
     print()
-    print(confusion_matrix(actual, predict))
+    cm = confusion_matrix(actual, predict)
+    cm_pct = []
+
+    for i in [0,1]:
+        cm_pct.append([cm[i][0]/sum(cm[i]),
+                        cm[i][1]/sum(cm[i])])
+    cm_pct = np.array(cm_pct)
+    cm_pct = np.round(cm_pct, 3)
+
+    print('>> Volume <<')
+    print()
+    print(pd.DataFrame(cm, columns=['Predict: 0','Predict: 1'],
+                            index=['Actual: 0', 'Actual: 1']))
+    print()
+    print('>> Percentage of Actual <<')
+    print()
+    print(pd.DataFrame(cm_pct, columns=['Predict: 0','Predict: 1'],
+                            index=['Actual: 0', 'Actual: 1']))
     print()
     
     predict_prob = pd.Series(predict_prob)
 
     print('-----Probability Distribution-----')
     print()
-    print('Class 0:')
-    print(predict_prob[actual==0].describe([0.2,0.4,0.6,0.8]))
-    print()
-    print('Class: 1')
-    print(predict_prob[actual==1].describe([0.2,0.4,0.6,0.8]))
+    
+    df = pd.DataFrame(predict_prob[actual==0].describe([0.2,0.4,0.6,0.8]).round(3),
+                        columns=['Class: 0'])
+
+    df = df.join(predict_prob[actual==1].describe([0.2,0.4,0.6,0.8]).round(3).rename('Class: 1'))
+
+    print(df)
 
     if prob_hist:
 
@@ -43,7 +63,5 @@ def summary(actual, predict_prob, thres=0.5, prob_hist=False):
         pyplot.hist(predict_prob[actual==1], bins, 
                     alpha=0.5, label='Class: 1', normed=True)
         pyplot.legend(loc='upper right')
-
         pyplot.axvline(x=thres)
-
         pyplot.show()
