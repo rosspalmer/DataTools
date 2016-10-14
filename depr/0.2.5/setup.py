b1425a5807c0d141313c005c40c3de454c68111e
Ross Palmer <palmer@opttek.com>
2016-10-14 11:18:58 -0600

    Clearing for 0.3 complete rewrite

---
+++ depr/0.2.5/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+config = {
+    'description':'Data Mining and Analysis Tools',
+    'author': 'ross palmer',
+    'license':'MIT',
+    'version': '0.2.5',
+    'install_requires': ['pandas','numpy'],
+    'packages': find_packages(),
+    'scripts': [],
+    'name':'dtools'
+}
+
+setup(**config)
